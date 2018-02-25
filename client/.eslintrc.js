1769b1c9d8782d12fa39ad64f84188b94cc43b3b
Vitao18 <victoronline9@gmail.com>
2018-02-25 00:07:45 -0300

    changed config enviroment to browser and ignore jsx format

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": "airbnb",
+    "env": {
+        "browser": true
+    },
+    "rules": {
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    }
 };
