a3fe47c68e4bfbd1dd2849d239d06ecfd925b3fa
Mariano Mariano <marianofheller@gmail.com>
2018-02-28 18:32:48 -0300

    Login.js renamed to index.js

--- client/src/components/Login/Login.test.js
+++ client/src/components/Login/Login.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { LoginComponent as Login } from './Login';
+import { LoginComponent as Login } from './index';
 
 Enzyme.configure({ adapter: new Adapter() });
 
