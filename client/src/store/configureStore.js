e106683224635679cfb03913d5c4050e9cdabaa4
Vitao18 <victoronline9@gmail.com>
2018-03-02 22:50:33 -0300

    refactored configStore and added router

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,8 +1,4 @@
 import devStore from './configureStore.dev';
 import prodStore from './configureStore.prod';
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = prodStore;
-} else {
-  module.exports = devStore;
-}
+export default process.env.NODE_ENV === 'production' ? prodStore : devStore;
