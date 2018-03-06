c7e245696fa89fbe032bc0f665a0e8ca5ee6e62f
Vitao18 <victoronline9@gmail.com>
2018-03-05 23:56:55 -0300

    added basic css

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import './css/index.css';
+
 const Navbar = () => (
   <ul>
     <li>Home</li>
     <li>Playlists</li>
-    <li>Logo</li>
+    <li className="logo">Logo</li>
     <li>Login/Signout</li>
     <li>Profile</li>
   </ul>
