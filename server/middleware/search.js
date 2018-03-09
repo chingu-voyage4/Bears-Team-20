88c07c8f570930eb47b07c83e885e4d871d70c09
Mariano Heller <marianofheller@gmail.com>
2018-03-09 10:34:45 -0300

    mw implemetation

--- server/middleware/search.js
+++ server/middleware/search.js
@@ -1,6 +1,26 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+const YouTube = require('youtube-node');
+
+const youTube = new YouTube();
+youTube.setKey(process.env.KEY_YOUTUBE);
 
 function mwYoutubeSearch(req, res, next) {
-    next();
+	const {searchQuery, maxResults} = req.body;
+    youTube.search(searchQuery, maxResults, (error, result) => {
+    	if (error) {
+            console.log(error);
+            res.locals.searchResults = {
+    			...res.locals.searchResults,
+    			youtube: null
+    		};
+    	} else {
+    		res.locals.searchResults = {
+    			...res.locals.searchResults,
+    			youtube: result
+    		};
+    	}
+        next();
+    });
 }
 
 module.exports = {
