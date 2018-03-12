d387592715e6ccdf01725c48e676065f1458d987
Vitao18 <victoronline9@gmail.com>
2018-03-12 16:07:17 -0300

    changed a tags to Link and changed eslint rule for Link

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => (
   <nav>
     <h1 className="logo">Logo</h1>
     <ul>
-      <li><a href="_blank"role="button">Home</a></li>
-      <li><a href="_blank"role="button">Playlists</a></li>
-      <li><a href="_blank"role="button">Login/Signout</a></li>
-      <li><a href="_blank"role="button">Profile</a></li>
+      <li><Link to="/home">Home</Link></li>
+      <li><Link to="/playlists">Playlists</Link></li>
+      <li><Link to="/login">Login/Signout</Link></li>
+      <li><Link to="/profile">Profile</Link></li>
     </ul>
   </nav>
 );
