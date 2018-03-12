40128a28750752a8b2b3bc64947b83f1f6653dcf
Vitao18 <victoronline9@gmail.com>
2018-03-11 22:56:43 -0300

    added anchor tag to list itens

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,10 +6,10 @@ const Navbar = () => (
   <nav>
     <h1 className="logo">Logo</h1>
     <ul>
-      <li>Home</li>
-      <li>Playlists</li>
-      <li>Login/Signout</li>
-      <li>Profile</li>
+      <li><a href="_blank"role="button">Home</a></li>
+      <li><a href="_blank"role="button">Playlists</a></li>
+      <li><a href="_blank"role="button">Login/Signout</a></li>
+      <li><a href="_blank"role="button">Profile</a></li>
     </ul>
   </nav>
 );
