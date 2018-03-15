a9a0ea74135bc3722e923c75f3e34a72f9b80529
Mariano Mariano <marianofheller@gmail.com>
2018-03-15 20:09:38 -0300

    init

---
+++ client/src/reducers/player.js
@@ -0,0 +1,13 @@
+
+
+
+const initialState = {
+    isPlaying: false,
+    isLooping: false,
+    volume: 1,
+    isMuted: false,
+    currentSong: null,
+    currentPlaylist: [],
+    errors: []
+};
+  
