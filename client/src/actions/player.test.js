7c2c4e988da0f666b264eba3098e6faf780eff0b
Mariano Heller <marianofheller@gmail.com>
2018-03-21 09:34:14 -0300

    actions tests

--- client/src/actions/player.test.js
+++ client/src/actions/player.test.js
@@ -40,4 +40,69 @@ describe('player actions', ()=> {
             type: actions.PLAYER_TOGGLE_LOOP
         })
     })
+
+    it('should return the correct type on playerSetVolume', () => {
+        const mockVolume = 1;
+        expect(actions.playerSetVolume(mockVolume)).toEqual({
+            type: actions.PLAYER_SET_VOLUME,
+            volume: mockVolume
+        })
+    })
+
+    it('should return the correct type on playerToggleMute', () => {
+        expect(actions.playerToggleMute()).toEqual({
+            type: actions.PLAYER_TOGGLE_MUTE
+        })
+    })
+
+    it('should return the correct type on playerPlaySong', () => {
+        const mockSong = {
+            title: "asdasd",
+            link: "aaaaaa.om"
+        };
+        expect(actions.playerPlaySong(mockSong)).toEqual({
+            type: actions.PLAYER_PLAY_SONG,
+            song: mockSong
+        })
+    })
+
+    it('should return the correct type on playerSetSong', () => {
+        const mockSong = {
+            title: "asdasd",
+            link: "aaaaaa.om"
+        };
+        expect(actions.playerSetSong(mockSong)).toEqual({
+            type: actions.PLAYER_SET_SONG,
+            song: mockSong
+        })
+    })
+
+    it('should return the correct type on playerSetPlaylist', () => {
+        const mockPlaylist = [
+            {
+                title: "asdasd",
+                link: "aaaaaa.om"
+            }
+        ]
+        expect(actions.playerSetPlaylist(mockPlaylist)).toEqual({
+            type: actions.PLAYER_SET_PLAYLIST,
+            playlist: mockPlaylist
+        })
+    })
+
+    it('should return the correct type on playerSetDuration', () => {
+        const mockDuration = 111;
+        expect(actions.playerSetDuration(mockDuration)).toEqual({
+            type: actions.PLAYER_SET_DURATION,
+            duration: mockDuration
+        })
+    })
+
+    it('should return the correct type on playerSetProgress', () => {
+        const mockProgress = 32;
+        expect(actions.playerSetProgress(mockProgress)).toEqual({
+            type: actions.PLAYER_SET_PROGRESS,
+            progress: mockProgress
+        })
+    })
 })
