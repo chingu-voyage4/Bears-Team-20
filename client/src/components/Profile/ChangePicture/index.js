3b7e3f07ad6dce9dcc7d4864623657650a7e5edc
Mariano Mariano <marianofheller@gmail.com>
2018-03-29 15:35:34 -0300

    Finished style stuff

--- client/src/components/Profile/ChangePicture/index.js
+++ client/src/components/Profile/ChangePicture/index.js
@@ -12,52 +12,27 @@ class ChangePictureComponent extends React.Component {
     super(props);
 
     this.state = {
+      editing: false,
       url: props.profilePic
     }
 
     this.handleUploadPicture = this.handleUploadPicture.bind(this);
     this.handleInnerLocalChange = this.handleInnerLocalChange.bind(this);
+    this.handleEditClick = this.handleEditClick.bind(this);
+  }
+
+  handleEditClick() {
+    this.setState({editing: true});
   }
 
   handleInnerLocalChange(newUrl) {
-    console.log("NEW URL", newUrl);
     this.setState({ url: newUrl });
   }
 
   handleUploadPicture() {
     const { changePicture } = this.props;
-    //changePicture();
-    confirmAlert({
-      customUI: ({ onClose }) => {
-        return (
-          <div id='change-pic-modal-container'>
-            <Typography
-            id="change-pic-modal-title"
-            variant="title"
-            color="primary"
-            >
-              Enter new picture's url
-            </Typography>
-            <Input
-            value={this.state.url}
-            onChange={ (e) => this.handleInnerLocalChange(e.target.value) }
-            />
-            <div id="change-pic-modal-buttons-container">
-              <Button onClick={onClose}>Cancel</Button>
-              <Button
-              color="primary"
-              onClick={() => {
-                onClose()
-              }}
-              >
-                Change it!
-              </Button>
-            </div>
-          </div>
-            
-        )
-      }
-    })
+    this.setState({editing: false});
+    changePicture(this.state.url);
   }
 
   render() {
@@ -82,19 +57,30 @@ class ChangePictureComponent extends React.Component {
             />
           </div>
 
+          <Input
+          id="change-pic-url-input"
+          disabled={!this.state.editing}
+          value={this.state.url}
+          onChange={ (e) => this.handleInnerLocalChange(e.target.value) }
+          />
+
           <div id="change-pic-controls-container">
             <Button
+              id="edit-pic-button"
+              disabled={this.state.editing}
+              color="primary"
+              onClick={this.handleEditClick}
+            >
+              Modify url
+            </Button>
+            <Button
               id="change-pic-button"
-                        /* disabled={this.props.isFetching} */
+              disabled={!this.state.editing}
               color="primary"
               onClick={this.handleUploadPicture}
             >
-              Change picture
+              Confirm changes!
             </Button>
-
-            <Typography>
-              You can also drag and drop a picture from your computer
-            </Typography>
           </div>
         </div>
       </div>
