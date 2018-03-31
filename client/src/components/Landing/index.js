506fd13e8e894b592b666cc3ef3e97273f32e611
Mariano Mariano <marianofheller@gmail.com>
2018-03-31 17:12:29 -0300

    merge development branch

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -5,25 +5,23 @@ import './Landing.css';
 //  const LandingPage = () => <h1>Landing Page!</h1>;
 
 
-const LandingPage = () => {
-  return (
-    <div className="box" >
-      <h1 id="head-title">Play music from everywhere</h1>
-      <img src="/cell.png" alt="" />
-      <p id="middle-title">Search, play, create, and share from anywhere</p>
-      <div>
-        <ul id="provider">
-          <li><img src="/icons/vimeo.png" alt="" /></li>
-          <li><img src="/icons/soundcloud.png" alt="" /></li>
-          <li><img src="/icons/youtube.png" alt="" /></li>
-          <li><img src="/icons/spotify.png" alt="" /></li>
-        </ul>
+const LandingPage = () => (
+  <div className="box" >
+    <h1 id="head-title">Play music from everywhere</h1>
+    <img src="/cell.png" alt="" />
+    <p id="middle-title">Search, play, create, and share from anywhere</p>
+    <div>
+      <ul id="provider">
+        <li><img src="/icons/vimeo.png" alt="" /></li>
+        <li><img src="/icons/soundcloud.png" alt="" /></li>
+        <li><img src="/icons/youtube.png" alt="" /></li>
+        <li><img src="/icons/spotify.png" alt="" /></li>
+      </ul>
 
-        <Link id="link" to="/login">Sign up for free</Link>
+      <Link id="link" to="/login">Sign up for free</Link>
 
-      </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default LandingPage;
